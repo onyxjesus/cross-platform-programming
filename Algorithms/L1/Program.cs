e4026f558d5518663bf8c2d4d5c7f0e481b7795f
Roman Andriiuk <66556314+onyxjesus@users.noreply.github.com>
2021-12-06 18:14:58 +0200

    Add files via upload

---
+++ Algorithms/L1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long N = Int64.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(N));
+        }
+        
+        public static int Calculate(long N)
+        {
+            if (N < 3) return 0;
+            if (N == 3) return 1;
+            if (N % 2 == 0) return 2 * Calculate(N / 2);
+            var k = N / 2;
+            return Calculate(k) + Calculate(k + 1);
+        }
+    }
+}
---
+++ Algorithms/L3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace L3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader inp = new StreamReader("input.txt");
+            int N = Int32.Parse(inp.ReadLine());
+            Dictionary<string, List<string>> G = new Dictionary<string, List<string>>();
+            
+            for (int i = 0; i < N; i++)
+            {
+                string key = inp.ReadLine();
+                G.Add(key, new List<string>());
+                
+                int k = Int32.Parse(inp.ReadLine());
+                for (int j = 0; j < k; j++)
+                    G[key].Add(inp.ReadLine());
+                inp.ReadLine();
+            }
+            foreach (var key in G.Keys)
+            {
+                var res = BFS(G, key);
+                Console.WriteLine(BFS(G, key) ? "YES":"NO");
+            }
+        }
+        public static bool BFS(Dictionary<string, List<string>> G, string k)
+        {
+            Dictionary<string, bool> isVisited = new Dictionary<string, bool>();
+            foreach (var key in G.Keys)
+            {
+                isVisited.Add(key, false);
+            }
+            Queue<string> q = new Queue<string>();
+            q.Enqueue(k);
+            isVisited[k] = true;
+            while (q.Count!=0)
+            {
+                var neighbours = G[q.Dequeue()];
+                foreach (var neighbour in neighbours)
+                {
+                    if (neighbour == k)
+                        return true;
+                    if (!isVisited[neighbour])
+                    {
+                        q.Enqueue(neighbour);
+                        isVisited[neighbour] = true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+}
